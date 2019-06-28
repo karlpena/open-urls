913b65c454924645f09fe7368f4a8d5f1ded778b
Karl Pena <karl@scientiamobile.com>
2019-06-27 23:51:21 -0400

    Updated path

--- open-urls-gnulinux.sh
+++ open-urls-gnulinux.sh
@@ -2,7 +2,7 @@
 # written for GNU/Linux, but if you want to adapt it for the *nixes (or other browsers, feel free to submit updates)
 while read line; do
 	# Choose a browser of your preference by uncommenting the line below for it, then comment-out the line with x-www-broser listed in it.
-##	google-chrome "$line"
-##	firefox "$line"
-	x-www-browser "$line"
+##	/usr/bin/google-chrome "$line"
+##	/usr/bin/firefox "$line"
+	/usr/bin/x-www-browser "$line"
 done < urls.txt
