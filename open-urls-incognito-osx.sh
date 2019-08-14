c6e747a34fbf32b715d32ccca11894c8a06c645c
karl <karl@karls-MBP.lan>
2019-08-14 19:46:44 -0400

    Disable application cache and aggressively discard the cache too.

--- open-urls-incognito-osx.sh
+++ open-urls-incognito-osx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # written for OSX (and Chrome), but if you want to adapt it for the *nixes (or other browsers, feel free to submit updates)
 while read line; do
-	open -a "Google Chrome.app" -n --args --incognito "$line"
+	open -a "Google Chrome.app" -n --args --incognito --disable-application-cache --aggressive-cache-discard "$line"
 done < urls.txt
