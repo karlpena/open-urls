6a375ff94c683a9af4680b6e64e13af089079c97
Karl Pena <9272108+karlpena@users.noreply.github.com>
2024-06-26 19:22:17 -0400

    Update open-urls-incognito-osx.sh

--- open-urls-incognito-osx.sh
+++ open-urls-incognito-osx.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#              Copyright (c) 2018 Karl G. Peña
+
 # written for OSX (and Chrome), but if you want to adapt it for the *nixes (or other browsers, feel free to submit updates)
 while read line; do
 	open -a "Google Chrome.app" -n --args --incognito --disable-application-cache --aggressive-cache-discard "$line"
